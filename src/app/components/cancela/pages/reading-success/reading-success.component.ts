5514ab578c1fa12c89d2eb500e8460ea283c8411
Naiara Duarte <naiara.correia@muralis.com.br>
2022-03-21 14:35:40 -0300

    tela de sucesso css

--- src/app/components/cancela/pages/reading-success/reading-success.component.ts
+++ src/app/components/cancela/pages/reading-success/reading-success.component.ts
@@ -6,24 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reading-success.component.scss']
 })
 export class ReadingSuccessComponent implements OnInit {
-  timer: any = 10;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.countdown()
   }
-
-  countdown() {
-    let interval = setInterval(() => {
-      this.timer--;
-      if (this.timer === 1) {
-        setTimeout(() => {
-          clearInterval(interval);
-         //TODO: Criar redirecionamento para a tela inicial
-        }, 1000);
-      }
-    }, 1000);
-  };
 
 }
